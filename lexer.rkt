92f20cbe811d1ebe2bb7399cd58b4bb0a1c0a724
Jianhan Wang <jianhanwang@MacBook-Pro-377.local>
2026-05-19 19:00:50 -0600

    update

--- lexer.rkt
+++ lexer.rkt
@@ -1,10 +1,71 @@
 #lang racket
-(define conditions '("if", "elif", "else"))
-(define logical_opeators ("and", "or", "not"))
-(define compairson_operators ("==", "!=", ">", "<"))
-(define loops '("while", "for"))
-(define operations '("(", ")", "**", "*", "/", "+", "-", "math.sqrt"))
+; Read the txt file
+(define file "Sample_Code.txt")
+(define in (open-input-file file))
+(define list (sequence->list (in-lines in)))
+(close-input-port in)
 
-(define (classify_type word)
+(for-each displayln list)
+
+(define conditions '("if" "elif" "else"))
+(define keywords '("print" "return"))
+(define logical_operators '("and" "or" "not"))
+(define comparison_operators '("==" "!=" ">" "<" ">=" "<="))
+(define loops '("while" "for"))
+(define operations '("**" "*" "/" "+" "-" "math.sqrt"))
+(define delimiters '("(" ")" "," ":"))
+(define builtins '("range" "len"))
+
+; Create token structure and disply actual values
+(struct token (type value) #:transparent)
+
+(define (split_line line)
+  (regexp-match* #px"[A-Za-z_][A-Za-z0-9_]*|\\d+|==|!=|<=|>=|\\*\\*|[:(),=+\\-*/<>]"
+                 line))
+
+;; Classify the word type
+(define (classify word)
   (cond
-    [(regexp-match
+    [(member word keywords)
+     (token 'KEYWORD word)]
+
+    [(member word operations)
+     (token 'OPERATION word)]
+
+    [(member word delimiters)
+     (token 'DELIMITER word)]
+
+    [(member word conditions)
+     (token 'CONDITION word)]
+
+    [(member word logical_operators)
+     (token 'LOGICAL_OPERATOR word)]
+
+    [(member word comparison_operators)
+     (token 'COMPARISON_OPERATOR word)]
+
+    [(member word loops)
+     (token 'LOOP word)]
+
+    [(regexp-match #px"^-?[0-9]+$" word) ; Handles negative & positive numbers
+     (token 'NUMBER word)]
+
+    [(regexp-match #px"^[A-Za-z_][A-Za-z0-9_]*$" word) ; Identifiers
+     (token 'IDENTIFIER word)]
+
+    [else
+     (token 'UNKNOWN word)]))
+
+; Tokenize the entire file
+(define (tokenize_line line)
+  (cond
+    [(regexp-match #px"^\\s*#" line)
+     (list (token 'COMMENT line))
+    ]
+    [else
+     (map classify (split_line line)) ]
+  )
+)
+
+(define all_tokens
+  (map tokenize_line list))
