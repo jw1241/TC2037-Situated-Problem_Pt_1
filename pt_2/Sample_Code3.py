b2ce8dfb905910cecaac959e3afd176a419f4d88
Jianhan Wang <jianhanwang@MacBook-Pro-377.local>
2026-06-12 22:15:47 -0600

    d

---
+++ pt_2/Sample_Code3.py
@@ -0,0 +1,42 @@
+import math
+
+x = 8
+y = 3 ** 2
+z = 4 * 5
+w = 20 / 4
+p = 9 + 1
+q = x + z + 12
+r = (p + 5)
+s = (3 * (2 + 4))
+t = 15 - 7
+u = 8 / 2 * (2 + 2) ** 2
+message = "Learning Python"
+numbers = [5,6,7,8]
+root = math.sqrt(9)
+
+print(1 + 3 * 2)
+print(y)
+print(z)
+print(w)
+print(p)
+print(t)
+print(u)
+print(message)
+print(numbers)
+print(root)
+
+for i in range(5):
+    print(i)
+
+counter = 3
+while counter > 0:
+    counter -= 1
+    print(counter)
+
+x = 8
+if x == 10:
+    print(x)
+elif x == 8:
+    print(x)
+else:
+    print(0)
---
+++ pt_2/Sample_Code4.py
@@ -0,0 +1,42 @@
+import math
+
+a = 10
+b = 2 ** 3
+c = 9 * 3
+d = 18 / 6
+e = 7 + 8
+f = a + c + 5
+g = (e + 10)
+h = (4 * (3 + 1))
+i = 20 - 6
+j = 10 / 2 * (2 + 1) ** 2
+text = "Parser Test"
+values = [10,20,30]
+k = math.sqrt(16)
+
+print(5 + 5 * 2)
+print(b)
+print(c)
+print(d)
+print(e)
+print(i)
+print(j)
+print(text)
+print(values)
+print(k)
+
+for n in range(3):
+    print(n)
+
+m = 5
+while m > 0:
+    m -= 1
+    print(m)
+
+a = 1
+if a == 2:
+    print(a)
+elif a == 1:
+    print(a)
+else:
+    print(99)
